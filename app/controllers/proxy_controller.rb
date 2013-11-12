9d60aa51b08ed30dea75aa51efe73441be09270a
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-12 11:07:31 -0800

    Can't seem to get Heroku to play nice, wtf

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,7 +1,9 @@
 class ProxyController < ApplicationController
   def proxy
-    require 'open-uri'
-    passthrough = open(params[:url]).read if params[:url].present?
-    render json: passthrough || {empty: true}, callback: params[:callback]
+    # require 'open-uri'
+    # passthrough = open(params[:url]).read if params[:url].present?
+    # render json: passthrough || {empty: true}, callback: params[:callback]
+
+    render text: "I give up"
   end
 end
