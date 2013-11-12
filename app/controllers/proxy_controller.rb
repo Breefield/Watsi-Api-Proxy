4e8a7337264db53ee6f2e69731346bc83deed6f5
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-12 11:22:25 -0800

    Why oh why Herok, what primary server are you even looking for

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,9 +1,7 @@
 class ProxyController < ApplicationController
   def proxy
-    # require 'open-uri'
-    # passthrough = open(params[:url]).read if params[:url].present?
-    # render json: passthrough || {empty: true}, callback: params[:callback]
-
-    render text: "I give up"
+    require 'open-uri'
+    passthrough = open(params[:url]).read if params[:url].present?
+    render json: passthrough || {empty: true}, callback: params[:callback]
   end
 end
